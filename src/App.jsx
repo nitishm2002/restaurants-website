08e9d07361cba33221ed2efeecd8255e36922738
Nitish <nitishm280@gmail.com>
2024-06-24 19:14:06 +0530

    `feat:` upload on github

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import PageNotFound from './pages/PageNotFound';
+import Menu from './pages/Menu';
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/menu' element={<Menu />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
